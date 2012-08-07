c7326dc40807b1369ffc5491eb64e301b9aa1ab1
mech <mech@me.com>
2012-08-07 12:44:46 +0800

    Use autoload

--- lib/smbglobal-sms.rb
+++ lib/smbglobal-sms.rb
@@ -1,6 +1,9 @@
 require "smbglobal_sms/version"
 require "smbglobal_sms/config"
 
+autoload :Request, 'smbglobal_sms/request'
+autoload :Response, 'smbglobal_sms/response'
+
 SmbglobalSms.configure do |config|
   config.host_name = "http://api.smbglobal.net"
   config.xml_unicode_sms_end_point = "/api/sendunicodesms2"
