88d3ca797275c46b84fa8c46a6ec79736009fe16
mech <mech@me.com>
2012-08-07 18:14:57 +0800

    More useful error message

--- lib/smbglobal_sms/error.rb
+++ lib/smbglobal_sms/error.rb
@@ -9,30 +9,51 @@ module SmbglobalSms
 
     # Status = -2 or -101
     class InvalidDataFormatError < StandardError
+      def message
+        "Data format incorrect"
+      end
     end
 
     # Status = -3
     class NotEnoughCreditsError < StandardError
+      def message
+        "No more credit!"
+      end
     end
 
     # Status = -4
     class InvalidRecipientError < StandardError
+      def message
+        "Please include country code like 65XXXXXXXX"
+      end
     end
 
     # Status = -5
     class ProcessingError < StandardError
+      def message
+        "Processing error"
+      end
     end
 
     # Status = -100
     class MissingParametersError < StandardError
+      def message
+        "Missing parameters"
+      end
     end
 
     # Status = -102
     class DuplicatedRequestError < StandardError
+      def message
+        "Duplicated request"
+      end
     end
 
     # Status = -103
     class ServiceUnavailableError < StandardError
+      def message
+        "SMS Service currently not available!"
+      end
     end
   end
 end
